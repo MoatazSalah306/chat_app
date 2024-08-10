c7c4f01e590cb1d0b5e7620a0e6ecd83548482d3
MoatazSalah306 <moatazsalah3062005@gmail.com>
2024-08-10 14:55:24 +0300

    apply filters in chatlist to filter conversations

--- app/Livewire/Chat/ChatList.php
+++ app/Livewire/Chat/ChatList.php
@@ -10,15 +10,18 @@ class ChatList extends Component
 {
     public $selectedConversation;
     public $query;
+    public $sortOrder = 'desc';
 
-
+    public function setSortOrder($order)
+    {
+        $this->sortOrder = $order;
+    }
 
 
     #[On("update-conversation")]
     public function render()
     {
-
-        $conversations = auth()->user()->conversations()->latest('updated_at')->get();
+        $conversations = auth()->user()->conversations()->orderBy('updated_at', $this->sortOrder)->get();
         return view('livewire.chat.chat-list', compact('conversations'));
     }
 }
--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -41,14 +41,51 @@
             <div class="flex items-center gap-2">
                  <h5 class="font-extrabold text-2xl ml-4">Chats</h5>
             </div>
+           
+             <x-dropdown align="right" width="48">
+                <x-slot name="trigger">
+                    <button>
 
-             <button>
+                        <svg class="w-7 h-7"  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+                          </svg>
 
-                <svg class="w-7 h-7"  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
-                  </svg>
+                    </button>
+                </x-slot>
 
-             </button>
+                <x-slot name="content">
+
+                    <div class="w-full p-1" x-data="{ sortOrder: @entangle('sortOrder') }">
+
+                        <a @click.prevent="$wire.setSortOrder('desc')" @class(['cursor-pointer items-center gap-3 flex w-full px-4 py-2 text-left text-sm leading-5 text-gray-500 hover:bg-gray-100 transition-all duration-150 ease-in-out focus:outline-none focus:bg-gray-100', 'bg-gray-100' => $sortOrder == 'desc'])>
+
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-down" viewBox="0 0 16 16">
+                                    <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z"/>
+                                  </svg>
+                            </span>
+
+                            Newest to oldest
+
+                        </a>
+
+                        <a @click.prevent="$wire.setSortOrder('asc')" @class(['cursor-pointer items-center gap-3 flex w-full px-4 py-2 text-left text-sm leading-5 text-gray-500 hover:bg-gray-100 transition-all duration-150 ease-in-out focus:outline-none focus:bg-gray-100', 'bg-gray-100' => $sortOrder == 'asc'])>
+
+                            <span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-up" viewBox="0 0 16 16">
+                                    <path d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z"/>
+                                </svg>
+                            </span>
+
+                            oldest to Newest
+
+                        </a>
+                 
+                        
+                    </div>
+        
+                </x-slot>
+            </x-dropdown>
 
         </div>
 
